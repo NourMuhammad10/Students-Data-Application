663cfed510826ab9f778a2d5c134ee7ab547a63b
NourMuhammad10 <101945184+NourMuhammad10@users.noreply.github.com>
2023-05-14 20:01:25 +0200

    Added MinHeap

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 #include "BinarySearchTree/BinarySearchTree.cpp"
+#include "MinHeap/MinHeap.cpp"
 #include "Student/Student.h"
 #include"LoadFile.cpp"
 using namespace std;
@@ -9,25 +10,26 @@ void BSTMenu();
 
 int main(){
     //displayMainMenu();
-    BinarySearchTree<int> tree;
-    tree.insert(5);
-    tree.insert(2);
-    tree.insert(12);
-    tree.insert(-4);
-    tree.insert(3);
-    tree.insert(9);
-    tree.insert(21);
-    tree.insert(19);
-    tree.insert(25);
-    tree.insert(18);
-    tree.insert(20);
-    tree.preorder(tree.getRoot());
-    cout<<endl;
-    cout<<tree.search(25)<<endl;
-    cout<<tree.search(7)<<endl;
-    tree.Delete(21);
-    tree.preorder(tree.getRoot());
+//    BinarySearchTree<int> tree;
+//    tree.insert(5);
+//    tree.insert(2);
+//    tree.insert(12);
+//    tree.insert(-4);
+//    tree.insert(3);
+//    tree.insert(9);
+//    tree.insert(21);
+//    tree.insert(19);
+//    tree.insert(25);
+//    tree.insert(18);
+//    tree.insert(20);
+//    tree.preorder(tree.getRoot());
+//    cout<<endl;
+//    cout<<tree.search(25)<<endl;
+//    cout<<tree.search(7)<<endl;
+//    tree.Delete(21);
+//    tree.preorder(tree.getRoot());
 }
+
 void displayMainMenu(){
     LoadFile studentData;
     vector<Student> students = studentData.loadFile();
