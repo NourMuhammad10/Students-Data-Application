c821b0b2b232fab84f8caec385a70b476e8ce015
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2023-05-15 01:17:04 +0300

    commit main for AVL

--- main.cpp
+++ main.cpp
@@ -28,22 +28,27 @@ int main(){
     AVL<int> tree;
 //    tree.insert(10);
 //    tree.insert(20);
-//    tree.insert(30);
-//    tree.insert(40);
-//    tree.insert(50);
-//    tree.insert(25);
+//    tree.insert(5);
+//    tree.insert(2);
+//    tree.insert(9);
+//    tree.insert(6);
+//    tree.deleteNode(5);
 
-
-//    tree.insert(40);
-//    tree.insert(30);
-//    tree.insert(50);
-//    tree.insert(25);
-//    tree.insert(35);
-//    tree.insert(15);
-//    tree.insert(28);
-//    tree.insert(45);
-//    tree.insert(60);
-//    tree.insert(55);
-//    tree.insert(70);
-//    tree.pre_Order(tree.getRoot());
+    tree.insert(40);
+    tree.insert(30);
+    tree.insert(50);
+    tree.insert(25);
+    tree.insert(35);
+    tree.insert(15);
+    tree.insert(28);
+    tree.insert(45);
+    tree.insert(60);
+    tree.insert(55);
+    tree.insert(70);
+    tree.deleteNode(35);
+    tree.deleteNode(40);
+    tree.deleteNode(28);
+    tree.deleteNode(25);
+    tree.pre_Order(tree.getRoot());
+    tree.deleteNode(50);
 }
